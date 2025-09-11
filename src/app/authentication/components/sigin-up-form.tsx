631b293d7c82d4dee732c38444412b7c5095a879
Luciolcjdev <luciodv83@gmail.com>
2025-09-11 10:54:39 -0300

    feat: add sign up and create accounts

--- src/app/authentication/components/sigin-up-form.tsx
+++ src/app/authentication/components/sigin-up-form.tsx
@@ -18,9 +18,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const registerSchema = z.object({
   name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
@@ -36,6 +39,7 @@ const registerSchema = z.object({
 });
 
 export default function SiginUpForm() {
+  const router = useRouter();
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
     defaultValues: {
@@ -45,9 +49,21 @@ export default function SiginUpForm() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof registerSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof registerSchema>) {
+    await authClient.signUp.email(
+      {
+        email: values.email,
+        password: values.password,
+        name: values.name,
+      },
+      {
+        onSuccess: () => {
+          router.push("/dashboard");
+        },
+      },
+    );
   }
+
   return (
     <Card>
       <Form {...form}>
@@ -90,7 +106,11 @@ export default function SiginUpForm() {
                 <FormItem>
                   <FormLabel>Senha</FormLabel>
                   <FormControl>
-                    <Input placeholder="Digite sua senha" {...field} />
+                    <Input
+                      type="password"
+                      placeholder="Digite sua senha"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -98,8 +118,16 @@ export default function SiginUpForm() {
             />
           </CardContent>
           <CardFooter>
-            <Button type="submit" className="w-full">
-              Criar conta
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={form.formState.isSubmitting}
+            >
+              {form.formState.isSubmitting ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                "Criar conta"
+              )}
             </Button>
           </CardFooter>
         </form>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,13 @@
 import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "pg" or "mysql"
     usePlural: true, // if your table names are plural (e.g. users, accounts)
+    schema,
   }),
   user: {
     modelName: "usersTable", // your users table name
@@ -18,5 +20,8 @@ export const auth = betterAuth({
   },
   verification: {
     modelName: "verificationsTable", // your verification table name
+  },
+  emailAndPassword: {
+    enabled: true,
   },
 });
