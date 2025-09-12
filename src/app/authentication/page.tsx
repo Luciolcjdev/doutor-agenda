82f036214ea3874698b5825cbf7ddaf1205b8fcc
Luciolcjdev <luciodv83@gmail.com>
2025-09-11 21:40:32 -0300

    feat: add custom error message for existing user

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -5,8 +5,8 @@ import SiginUpForm from "./components/sigin-up-form";
 
 export default function AuthenticationPage() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Tabs defaultValue="login" className="w-[480px">
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-500">
+      <Tabs defaultValue="login" className="w-[480px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="register">Criar Conta</TabsTrigger>
