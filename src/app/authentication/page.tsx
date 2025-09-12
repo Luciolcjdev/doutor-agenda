63b6a3e2e05a8f6cd4a776acd016f7173064a475
Luciolcjdev <luciodv83@gmail.com>
2025-09-11 23:57:46 -0300

    feat: implement authentication and dashboard session handling

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -2,8 +2,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import LoginForm from "./components/login-form";
 import SiginUpForm from "./components/sigin-up-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AuthenticationPage() {
+export default async function AuthenticationPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-slate-500">
       <Tabs defaultValue="login" className="w-[480px]">
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,24 @@
-export default function DashboardPage() {
-  return <div>DashboardPage - Protected Route</div>;
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import SignOutButton from "./components/sign-out-button";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    return redirect("/authentication");
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h2>Welcome {session?.user.name}</h2>
+      <p>Cadastrado com o e-mail: {session?.user.email}</p>
+      <div>
+        <SignOutButton />
+      </div>
+    </div>
+  );
 }
