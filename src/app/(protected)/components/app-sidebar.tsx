6b0d8198cbd86d5b95b2dc8553c71dd73a7947bc
Luciolcjdev <luciodv83@gmail.com>
2025-09-16 01:11:32 -0300

    fix: sidebar clinicId and clinicName

--- src/app/(protected)/components/app-sidebar.tsx
+++ src/app/(protected)/components/app-sidebar.tsx
@@ -105,7 +105,7 @@ export function AppSidebar() {
                     <AvatarFallback>F</AvatarFallback>
                   </Avatar>
                   <div>
-                    <p className="text-sm">{session.data?.user.clinic.name}</p>
+                    <p className="text-sm">{session.data?.user.clinic?.name}</p>
                     <p className="font-sm text-muted-foreground">
                       {session.data?.user.email}
                     </p>
